2a89affe07ba0cb4ca116c2448e3e702b1eeae17
saraelallouche <saraelallouche@hotmail.fr>
2025-03-26 19:07:38 +0100

    change debug setting and test package.json error rimraf

--- base/base/settings.py
+++ base/base/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'fallback-secret-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DJANGO_DEBUG', 'True') == 'True'
+DEBUG = os.getenv('DJANGO_DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', 'localhost').split(',')
 
