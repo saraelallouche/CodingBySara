9ee25973198af5411070f099014bd2de4aa8b087
saraelallouche <saraelallouche@hotmail.fr>
2025-08-11 21:03:55 +0200

    update STATIC_URL prod

--- config/settings.py
+++ config/settings.py
@@ -187,7 +187,7 @@ else:
     AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
 
     STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-    STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/static/"
+    STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/"
 
 
 # Default primary key field type
