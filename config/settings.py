bdc9e1206f87194672bfd35d94debde8210ac10b
saraelallouche <saraelallouche@hotmail.fr>
2025-08-12 01:34:30 +0200

    add AWS_LOCATION

--- config/settings.py
+++ config/settings.py
@@ -185,6 +185,7 @@ else:
     AWS_ACCESS_KEY_ID = env("AWS_ACCESS_KEY_ID")
     AWS_SECRET_ACCESS_KEY = env("AWS_SECRET_ACCESS_KEY")
     AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
+    AWS_LOCATION = "static"
 
     STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
     STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/"
