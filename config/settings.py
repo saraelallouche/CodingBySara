1fe3fae354f979fa9d34ca6d8e8067d1bc763966
saraelallouche <saraelallouche@hotmail.fr>
2025-08-12 01:43:18 +0200

    delete AWS_DEFAULT_ACL = "public-read"

--- config/settings.py
+++ config/settings.py
@@ -186,7 +186,6 @@ else:
     AWS_SECRET_ACCESS_KEY = env("AWS_SECRET_ACCESS_KEY")
     AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
     AWS_LOCATION = "static"
-    AWS_DEFAULT_ACL = "public-read"
 
     STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
     STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/"
